1e5bb9515aad7e48dd63ebf334b65b284f8f090d
gperezalba <gperezalba94@gmail.com>
2020-07-03 21:34:43 +0200

    solve mistake when updating offer

--- src/offer.ts
+++ src/offer.ts
@@ -133,13 +133,10 @@ export function updateOfferCommodity(event: UpdateOfferCommodity): void {
             offer.isOpen = false;
         } else {
             let gold = Gold.load(commodityId);
-            offer.buyAmount = event.params.buyAmount;
             offer.price_per_brute_weight = event.params.buyAmount.times(getOneEther()).div(gold.weight_brute as BigInt);
             offer.price_per_fine_weight = event.params.buyAmount.times(getOneEther()).div(gold.weight_fine as BigInt);
         }
     }
-
-    
 
     offer.save();
 }
