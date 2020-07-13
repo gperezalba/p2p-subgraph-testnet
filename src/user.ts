7b2ac1168e82716a6e349aa2f0c0fe865adb3e2d
gperezalba <gperezalba94@gmail.com>
2020-07-13 16:02:21 +0200

    update

--- src/user.ts
+++ src/user.ts
@@ -90,7 +90,7 @@ export function createUserIfNull(userId: string): void {
 }
 
 export function getNickname(walletAddress: string): string {
-    let nameService = NameService.bind(Address.fromString("0xFeAA9226a6d155fb46a18dAfbde12f4b3EF3c977"));
+    let nameService = NameService.bind(Address.fromString("0x6eF6DeD453c1e82B0B7ebC082E0Be4b6482F7fdC"));
     let name = nameService.try_name(Address.fromString(walletAddress));
 
     if (!name.reverted) {
